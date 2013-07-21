66a8fc3d79d1c5dfcde43f35bff33adb6e59565d
Sharad Bagri <sharad.bagri@gmail.com>
2013-07-21 23:56:31 +0000

    edit 4

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(32);
 app.get('/', function(request, response) {
     response.send(buf.toString(fs.readFileSync(index.html)));
-}
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
